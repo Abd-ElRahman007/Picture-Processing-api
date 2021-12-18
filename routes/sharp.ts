d6a9190ee8eb595c5807a1acd09216e135a0b532
Abd El-Rahman Adel <abdostarhero@hotmail.com>
2021-12-18 16:35:23 +0200

    apply prettier to the files

--- routes/sharp.ts
+++ routes/sharp.ts
@@ -1,24 +1,36 @@
 import express from 'express';
 import sharp from 'sharp';
-import { promises as fsPromises } from 'fs'
+import { promises as fsPromises } from 'fs';
 import fs from 'fs';
 
 const routes = express.Router();
 
 routes.get('/', (req: express.Request, res: express.Response) => {
   if (fs.existsSync(`./src/img/${req.query.filename}.jpg`)) {
-    if (fs.existsSync(`./src/thumbnail/${req.query.filename}_thumb${req.query.width}x${req.query.height}.jpg`)) {
-      res.sendFile(`${req.query.filename}_thumb${req.query.width}x${req.query.height}.jpg`, { root: './src/thumbnail' });
+    if (
+      fs.existsSync(
+        `./src/thumbnail/${req.query.filename}_thumb${req.query.width}x${req.query.height}.jpg`
+      )
+    ) {
+      res.sendFile(
+        `${req.query.filename}_thumb${req.query.width}x${req.query.height}.jpg`,
+        { root: './src/thumbnail' }
+      );
     } else {
-      fsPromises.mkdir('./src/thumbnail', { recursive: true })
+      fsPromises.mkdir('./src/thumbnail', { recursive: true });
       sharp(`./src/img/${req.query.filename}.jpg`)
         .resize(Number(req.query.width), Number(req.query.height))
-        .toFile(`./src/thumbnail/${req.query.filename}_thumb${req.query.width}x${req.query.height}.jpg`)
-        .then(info => {
+        .toFile(
+          `./src/thumbnail/${req.query.filename}_thumb${req.query.width}x${req.query.height}.jpg`
+        )
+        .then((info) => {
           console.log(info);
-          res.sendFile(`${req.query.filename}_thumb${req.query.width}x${req.query.height}.jpg`, { root: './src/thumbnail' });
+          res.sendFile(
+            `${req.query.filename}_thumb${req.query.width}x${req.query.height}.jpg`,
+            { root: './src/thumbnail' }
+          );
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
         });
     }
